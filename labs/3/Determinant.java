7965b665b2ff74fa13aed873c2d609510a6cec7b
Miguel Nistal <nistam328@gmail.com>
2018-02-28 12:25:28 -0500

    Added documentation for lab3 part1 (finding the determinant of a modular matrix)

--- labs/3/Determinant.java
+++ labs/3/Determinant.java
@@ -1,5 +1,6 @@
 /** 
  * Implementation of Lab 3 Part 1
+ * Calculating the Determinant of any Square Modular Matrix
  * @author Miguel Nistal 
  */
 
@@ -32,6 +33,12 @@ class Determinant {
 
   }
 
+  /** 
+   * Cofactor Expansion along the first row to get the Determinant of a Moduluar Matrix
+   *  @param modulo  The modulo of the matrix 
+   *  @param matrix  The matrix to find the determinant of
+   *  @return Modular Determinant 
+   */
   static int cofModDet(int modulo, int[][] matrix) {
     // Can use the outer length as long as the 2D Array is Square
     int matrix_size = matrix.length;
@@ -51,15 +58,22 @@ class Determinant {
     }
 
     // *Recursive Case* Matrix of any size greater than 2
-    // 
+    // Here we can use Co-factor expansion, take the cofactor and multiply it
+    // against a call to this function to get the determinant for the co-factor matrix 
+    // with a sufficiently small cofactor matrix (2x2), we'll return back the determinants 
     else {
+
+      // Cofactor matrix is N-1 x N-1
       int cofactor_matrix_size = matrix_size - 1;
 
-      // For each column along the first row
+      // Iterate across the first row of the matrix, the entries in the first row are the "cofactors"
+      // this is referred to as "cofactor expansion along the first row"
       for (int cofactor_index = 0; cofactor_index < matrix_size; cofactor_index++) {
         int[][] cofactor_matrix = new int[cofactor_matrix_size][cofactor_matrix_size];
 
-        // Skip the first row always, so we start at 1 and iterate to matrix_size 
+        // Since this expands cofactors along the first row, we never use Row 0 in our cofactor matrix
+        // We then have to iterate across ALL columns in the matrix and only skip the one that corresponds 
+        // to which cofactor we are currently working with (column == cofactor_index)
         int cofactor_column = 0;
         for (int row = 1; row < matrix_size; row++) {
           for (int column = 0; column < matrix_size; column++) {
@@ -70,12 +84,14 @@ class Determinant {
         }
 
         // The determinant is the sum of the cofactor expansions:
-        //             -1 ^ (n)  *  COFactor  * Determinant of the Cofactor Matrix 
+        //             -1 ^ (n)  *  Co-factor  * Determinant of the Co-factor Matrix 
         determinant += (Math.pow(-1, cofactor_index)) * matrix[0][cofactor_index] * cofModDet(modulo, cofactor_matrix);
       }
 
     }
-
+    
+    // *NOTE* The % operator is NOT Modulo in Java, it is REMAINDER
+    //         therefore we have to use a function from the Math lib
     return Math.floorMod(determinant, modulo);
   }
 
