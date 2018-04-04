342c425f1a55d4e8bcdde998bfa475f5e31bd55f
Miguel Nistal <nistam328@gmail.com>
2018-04-03 22:37:25 -0400

    Updated driver for lab5 to behave as described in the worksheet

--- labs/5/DriverAES.java
+++ labs/5/DriverAES.java
@@ -8,21 +8,19 @@ import java.util.Scanner;
 
 class DriverAES {
 
-  /** Driver to test of AES Implementation  Encryption */
+  /** 
+   * Driver to test of AES Implementation  Encryption 
+   * Expects Key and plaintext on lines 1 and 2
+   * Prints ciphertext to stdout 
+   */
   public static void main(String[] args) {
-
     Scanner input = new Scanner(System.in);
     String encryption_key = input.nextLine();
     String plaintext_block = input.nextLine();
     input.close();
 
-    System.out.println(" Key: " + encryption_key);
-    System.out.println(" P:   " + plaintext_block);
-    System.out.println();
-
-    //String result = AESCipher.AESEncryptBlock(plaintext_block, encryption_key);
-    //System.out.println(result);
-    AESUnitTests();
+    String result = AESCipher.AESEncryptBlock(plaintext_block, encryption_key);
+    System.out.println(result);
   }
 
   /** 
